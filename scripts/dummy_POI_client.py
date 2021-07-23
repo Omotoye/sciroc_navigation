78ec61cf345a4450062b67543f6111aa46f5b0fb
omotoye <adekoyaomotoye@gmail.com>
2021-07-23 13:59:43 +0200

    minor changes to the dummy client

--- scripts/dummy_POI_client.py
+++ scripts/dummy_POI_client.py
@@ -7,13 +7,14 @@ from sciroc_navigation.srv import GoToPOI
 def call_POI_service():
 	rospy.wait_for_service('go_to_poi_service')
 	try:
+		poi_ = raw_input("Enter a Valid Point of Interest: ")
 		go_to_poi = rospy.ServiceProxy('go_to_poi_service', GoToPOI)
-		result = go_to_poi('table')
+		result = go_to_poi(poi_)
 
 		if (result.result == 'goal reached'):
 			print result.result 
 		else: 
-			print 'Point of interest does not exist'
+			print 'Point of interest [{poi}] does not exist'.format(poi=poi_)
 	except rospy.ServiceException as e:
 		print 'Service call failed: {e}'.format(e=e)
 
